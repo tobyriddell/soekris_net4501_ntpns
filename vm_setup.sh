da4f72ef84417c4f80ef95e3382cf27d8da0d434
Toby Riddell <toby.riddell@prevtec.com>
2019-03-26 11:32:24 -0400

    Added skeleton for new file

---
+++ vm_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+make -C /usr/ports/devel/subversion install clean BATCH=yes
+make -C /usr/ports/sysutils/screen install clean BATCH=yes
+make -C /usr/ports/devel/configkit install clean BATCH=yes
+make -C /usr/ports/lang/tcl84 install clean BATCH=yes
+make -C /usr/ports/ftp/wget install clean BATCH=yes
+make -C /usr/ports/shells/bash install clean BATCH=yes
+make -C /usr/ports/lang/perl5.24 install clean BATCH=yes
+
+svn co svn://svn.freebsd.org/base/releng/11.1 /usr/src
+
+ftp http://phk.freebsd.dk/NTPns/phkrel/NTPns.20080902.tgz
+ftp http://phk.freebsd.dk/NTPns/phkrel/Libphk.20080902.tgz
