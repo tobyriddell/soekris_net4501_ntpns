7535709e942365125b46176d3570265dacc301c8
Toby Riddell <toby.riddell@prevtec.com>
2019-03-28 05:19:54 -0400

    Added instructions for building libphk

--- build_ntpns.sh
+++ build_ntpns.sh
@@ -7,3 +7,8 @@ ftp http://phk.freebsd.dk/NTPns/phkrel/Libphk.20080902.tgz
 tar -zxf NTPns.20080902.tgz
 tar -zxf Libphk.20080902.tgz
 
+# Compile libphk
+export DESTDIR=/usr/obj/nanobsd.net4501_ntpns_1.0
+(cd Libphk && make && make install)
+
+# Apply patches
