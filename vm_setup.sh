f7b030dd90a11495d9ad443460977c93d5bd5c6b
Toby Riddell <toby.riddell@prevtec.com>
2019-03-28 05:07:41 -0400

    Added comment, removed download commands

--- vm_setup.sh
+++ vm_setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-set -e
+# If anything fails to work as expected then exit
+set -e 
 
 make -C /usr/ports/devel/subversion install clean BATCH=yes
 make -C /usr/ports/sysutils/screen install clean BATCH=yes
@@ -11,6 +12,3 @@ make -C /usr/ports/shells/bash install clean BATCH=yes
 make -C /usr/ports/lang/perl5.24 install clean BATCH=yes
 
 svn co svn://svn.freebsd.org/base/releng/11.1 /usr/src
-
-ftp http://phk.freebsd.dk/NTPns/phkrel/NTPns.20080902.tgz
-ftp http://phk.freebsd.dk/NTPns/phkrel/Libphk.20080902.tgz
