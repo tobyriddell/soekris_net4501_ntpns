f212ab24d3a0938246abfb215f14b30a93c3ba8c
BuildTools <unconfigured@null.spigotmc.org>
2019-04-05 04:38:10 -0400

    Added minicom

--- vm_setup.sh
+++ vm_setup.sh
@@ -10,5 +10,6 @@ make -C /usr/ports/lang/tcl84 install clean BATCH=yes
 make -C /usr/ports/ftp/wget install clean BATCH=yes
 make -C /usr/ports/shells/bash install clean BATCH=yes
 make -C /usr/ports/lang/perl5.24 install clean BATCH=yes
+make -C /usr/ports/comms/minicom install clean BATCH=yes
 
 svn co svn://svn.freebsd.org/base/releng/11.1 /usr/src
